ccb299e4dd75ab616ef836c540000437bb3e5bb4
Alumno Suizo <alumno@ET26.DOMINIO>
2026-09-02 13:12:50 -0300

    cliente controllers

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -1,16 +1,109 @@
 const { Cliente } = require('../models/index.js')
 
-const mostrarCliente = async(req, res) =>{ //nombrar la funcion 
-    try{    //try / intento para usar las siguientes cosas
-        const clientesEncontrados = await Cliente.findAll();
-        if(!clientesEncontrados){
-            res.status(401).json({message: "No se encontró el cliente la ocncha de la lora"});
+const mostrarCliente = async(req, res) =>{
+    
+    try{
+
+        const listaClientes = await Cliente.findAll();
+
+        if(!listaClientes) {
+            return res.status(404).json({ message: "No se encontro ningun cliente" })
         }
-        res.status(200).json(clientesEncontrados);
+
+        res.status(200).json({listaClientes})
+
+    } catch(error) {
+        res.status(500).json({ error: error.message })
     }
-    catch(error){
-        res.status(401).json({error: error.message});
-    }
+
 }  
 
-module.exports = { mostrarCliente };
+const clientePorId = async(req, res) =>{
+
+    try{
+
+        const id = Number(req.params.id)
+        const listaClientes = await Cliente.findByPk(id)
+
+        if(!listaClientes) {
+            return res.status(404).json({ message: "No se encontro al cliente solicitado" })
+        }
+
+        res.status(200).json({listaClientes})
+
+    } catch(error) {
+        res.status(500).json({ error: error.message })
+    }
+
+}
+
+const registrarCliente = async(req, res) => {
+
+    try{
+
+        const { nombre, apellido, telefono } = req.body;
+        if(!nombre || !apellido || !telefono) {
+            return res.status(400).json({ message: "Falta rellenar parametros" })
+        }
+
+        const nuevoCliente = await Cliente.create({ nombre, apellido, telefono })
+
+        res.status(200).json({ message: "Cliente creado", cliente: nuevoCliente })
+
+    } catch(error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+const modificarCliente = async(req, res) => {
+    
+    try{
+
+        const id = Number(req.params.id);
+        const { nombre, apellido, telefono } = req.body;
+
+        const clienteModif = await Cliente.findByPk(id);
+
+        if(!clienteModif) {
+            return res.status(404).json({ message: "Cliente no encontrado" })
+        }
+
+        if(!nombre || !apellido || !telefono) {
+            return res.status(400).json({ message: "Faltan parametros" })
+        }
+
+        clienteModif.nombre = nombre;
+        clienteModif.apellido = apellido;
+        clienteModif.telefono = telefono;
+        await clienteModif.save();
+
+        res.status(200).json({ message: "Cliente modificado", cliente: clienteModif})
+
+    } catch(error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+const eliminarCliente = async(req, res) => {
+
+    try{
+
+        const id = Number(req.params.id);
+
+        const eliminarCliente = await Cliente.findByPk(id);
+
+        if(!eliminarCliente) {
+            return res.status(404).json({ message: "No se encontro al cliente" })
+        }
+
+        await eliminarCliente.destroy();
+
+        res.status(200).json({message: "Cliente eliminado con exito"})
+
+    }catch(error){
+        res.status(500).json({ error: error.message })
+    }
+
+}
+
+module.exports = { mostrarCliente, clientePorId, registrarCliente, modificarCliente, eliminarCliente };
--- routes/clienteRoutes.js
+++ routes/clienteRoutes.js
@@ -1,6 +1,10 @@
 const { Router } = require('express');
-const { mostrarCliente } = require('../controllers/clienteController.js');
+const { mostrarCliente, clientePorId, registrarCliente, modificarCliente, eliminarCliente } = require('../controllers/clienteController.js');
 const routes = Router() ;
 routes.get ('/', mostrarCliente);
+routes.get ('/:id', clientePorId);
+routes.post('/', registrarCliente);
+routes.put('/:id', modificarCliente);
+routes.delete('/:id', eliminarCliente);
 
 module.exports = routes;
